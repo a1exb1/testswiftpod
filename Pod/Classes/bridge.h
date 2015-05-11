83db4e5dca7ed00d1476a0272ad8ee912b7cbd2f
Alex Bechmann <alex_bechmann@hotmail.com>
2015-05-11 14:55:07 +0200

    bridge

---
+++ Pod/Classes/bridge.h
@@ -0,0 +1,17 @@
+//
+//  bridge.h
+//  Pods
+//
+//  Created by Alex Bechmann on 11/05/2015.
+//
+//
+
+#import "Encryptor.h"
+#import "Compressor.h"
+
+#ifndef Pods_bridge_h
+#define Pods_bridge_h
+
+
+
+#endif
